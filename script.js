ec0f1bfb78edb973f82335865446459de6815645
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-02-28 11:12:02 +0530

    Prime Number

--- script.js
+++ script.js
@@ -53,6 +53,8 @@ let sum = function (arr) {
 sum([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log("sum of all numbers:", frst);
 
+// IIFE Function
+
 let first = 0;
 (function (sum) {
   sum.forEach((sum) => {
@@ -60,3 +62,40 @@ let first = 0;
   });
 })([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log("sum of all numbers:", first);
+
+// Return all the prime numbers in an array
+
+// Anonymous Function
+
+let prime = function (num) {
+  for (let i = 0; i < num.length; i++) {
+    let flag = 0;
+    for (let j = 2; j < num[i]; j++) {
+      if (num[i] % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0) {
+      console.log("prime Number:", num[i]);
+    }
+  }
+};
+prime([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// IIFE Function
+
+(function (number) {
+  for (let i = 0; i < number.length; i++) {
+    let flag = 0;
+    for (let j = 2; j < number[i]; j++) {
+      if (number[i] % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0) {
+      console.log("prime Number:", number[i]);
+    }
+  }
+})([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
